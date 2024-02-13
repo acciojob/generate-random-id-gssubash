dff8c4233bc4d4d2e361bd4b9491645aec41d5a4
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-02-13 15:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function makeid(l) {
   // write your code here
-}
+	let alph = 'aAbbcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ';
+	let nums = '0123456789';
+	let max = alph.length-1,min=0,max2=nums.length-1,min2=0;
+	let flag = true;
+	let ans = '';
+	for(let i=0;i<l;i++){
+		let rand = Math.random();
+		let index = Math.floor(rand*(max-min+1)+min);
+		let index2 = Math.floor(rand*(max2-min2+1)+min2);
+		if(flag){
+			flag=false;
+			ans += alph[index];
+		}
+		else
+		{
+			ans+=nums[index2];
+			flag = true;
+		}
+	}
+	return ans;
+} 
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
